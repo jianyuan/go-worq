0206d484b8d53591fad43ea239ae7f3e3e81e114
Jian Yuan Lee <jianyuan@gmail.com>
2018-05-30 15:14:02 +0100

    amqpTableStringOk

--- protocols/celery/celery.go
+++ protocols/celery/celery.go
@@ -3,6 +3,8 @@ package celery
 import (
 	"errors"
 
+	"github.com/streadway/amqp"
+
 	worq "github.com/jianyuan/go-worq"
 	"github.com/jianyuan/go-worq/brokers/amqpbroker"
 )
@@ -21,13 +23,20 @@ func New() *Protocol {
 	return &Protocol{}
 }
 
+func amqpTableStringOk(t amqp.Table, key string) (string, bool) {
+	if value, ok := t[key]; ok {
+		if value, ok := value.(string); ok {
+			return value, true
+		}
+	}
+	return "", false
+}
+
 func (c *Protocol) ID(msg worq.Message) (string, error) {
 	switch msg := msg.(type) {
 	case *amqpbroker.Message:
-		if id, ok := msg.Delivery().Headers["id"]; ok {
-			if id, ok := id.(string); ok {
-				return id, nil
-			}
+		if id, ok := amqpTableStringOk(msg.Delivery().Headers, "id"); ok {
+			return id, nil
 		}
 	}
 	return "", ErrIDNotFound
@@ -36,10 +45,8 @@ func (c *Protocol) ID(msg worq.Message) (string, error) {
 func (c *Protocol) Task(msg worq.Message) (string, error) {
 	switch msg := msg.(type) {
 	case *amqpbroker.Message:
-		if id, ok := msg.Delivery().Headers["task"]; ok {
-			if id, ok := id.(string); ok {
-				return id, nil
-			}
+		if task, ok := amqpTableStringOk(msg.Delivery().Headers, "task"); ok {
+			return task, nil
 		}
 	}
 	return "", ErrTaskNotFound
