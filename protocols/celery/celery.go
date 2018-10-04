272ad851f5bc725a8ad4f7a509098febdb55eaad
Jian Yuan Lee <jianyuan@gmail.com>
2018-10-04 01:55:40 +0100

    Binder.Bind: Positional arguments support

--- protocols/celery/celery.go
+++ protocols/celery/celery.go
@@ -3,6 +3,7 @@ package celery
 import (
 	"encoding/json"
 	"errors"
+	"reflect"
 
 	"github.com/streadway/amqp"
 
@@ -62,12 +63,46 @@ func NewBinder() *Binder {
 }
 
 func (Binder) Bind(ctx worq.Context, v interface{}) error {
+	rv := reflect.ValueOf(v)
+	rt := reflect.TypeOf(v)
+
+	if rt == nil {
+		return errors.New("worq: Bind(nil)")
+	}
+
+	if rv.IsNil() {
+		return errors.New("worq: Bind(nil " + rt.String() + ")")
+	}
+
+	if rv.Kind() != reflect.Ptr {
+		return errors.New("worq: Bind(non-pointer " + rt.String() + ")")
+	}
+
 	msg := ctx.Message()
 	switch msg.ContentType() {
 	case MIMEApplicationJSON:
 		var body TaskBody
 		if err := json.Unmarshal(msg.Body(), &body); err != nil {
 			return err
+		}
+
+		var args []json.RawMessage
+		if err := json.Unmarshal(body[0], &args); err != nil {
+			return err
+		}
+
+		if len(args) > 0 {
+			if rt.Elem().Kind() != reflect.Struct {
+				return errors.New("worq: Bind(pointer to non-struct " + rt.String() + ")")
+			}
+
+			// TODO: Validation
+			for i, arg := range args {
+				f := rv.Elem().Field(i)
+				if err := json.Unmarshal(arg, f.Addr().Interface()); err != nil {
+					return err
+				}
+			}
 		}
 
 		// TODO: process position args
